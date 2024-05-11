79441afd4578a39f88dc80252e9b6b94dda897db
Litlery <Lazarevich.danil@yandex.ru>
2024-05-11 20:55:01 +0900

    class Player update, add move.

--- main.py
+++ main.py
@@ -2,8 +2,45 @@ import pygame
 
 
 class Player:
-    def __init__(self, width, height):
-        pass
+    def __init__(self, screen_width, screen_height):
+        self.__sprite = pygame.transform.smoothscale(
+            pygame.image.load("sprites/space_ship.png").convert_alpha(),
+            (screen_width // 4, screen_height // 10)
+        )
+
+        self.__rect = self.__sprite.get_rect()
+
+        self.__rect.x = screen_width // 2 - self.__rect.width // 2
+        self.__rect.y = screen_height - 80   # screen_height // 2 - self.__rect.height // 2
+
+        self.__horizontal_move_flag = 0
+        self.__speed = 30
+
+    def check_event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                self.__horizontal_move_flag = -1
+            elif event.key == pygame.K_d:
+                self.__horizontal_move_flag = 1
+
+
+    def check_logic(self, screen_width, screen_height):
+        if self.__rect.y < 0:
+            self.__rect.y = 0
+        elif self.__rect.y > screen_height - self.__rect.height:
+            self.__rect.y = screen_height - self.__rect.height
+
+    def move(self):
+        self.__rect.x += self.__speed * self.__horizontal_move_flag
+
+        self.__horizontal_move_flag = 0
+
+
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 255, 255), self.__rect)
+        screen.blit(self.__sprite, self.__rect)
+
 
 # Класс Игры
 class Game:
@@ -11,6 +48,7 @@ class Game:
     def __init__(self, width=420, height=700, bg_color=(200, 200, 200), fps=60):
 
         pygame.init()
+
 
         self.__width = width
         self.__height = height
@@ -21,15 +59,14 @@ class Game:
             (self.__width, self.__height)
         )
         self.__scene = 0
-
         self.__font1 = pygame.font.SysFont("Arial", 36)
         self.__test_text = self.__font1.render("TEST", True, (255, 255, 255))
-
         self.__fps = fps
         self.__clock = pygame.time.Clock()
 
         self.__game_end = False
 
+        self.__player = Player(self.__width, self.__height)
     def __del__(self):
         pygame.quit()
 
@@ -38,9 +75,15 @@ class Game:
         '''Метод для запуска игры'''
         while not self.__game_end:
             self.__check_events()
+            self.__check_logic()
+            self.__move_objects()
             self.__draw()
 
+    def __check_logic(self):
+        self.__player.check_logic(self.__width, self.__height)
 
+    def __move_objects(self):
+        self.__player.move()
 
     # Проверка событий
     def __check_events(self):
@@ -63,6 +106,7 @@ class Game:
 
             if event.type == pygame.QUIT:
                 self.__game_end = True
+            self.__player.check_event(event)
 
     # Отрисовка
     def __draw(self):
@@ -74,6 +118,8 @@ class Game:
 
         elif self.__scene == 2:
             self.__screen.blit(self.__test_text, (210, 350))
+
+        self.__player.draw(self.__screen)
         pygame.display.flip()
 
 def main():
